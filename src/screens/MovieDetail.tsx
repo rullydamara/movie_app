1cf3cddfc8f3c87bdf19da00d5a75a3c3f6c6008
Robby Ihza Mahendra <robbyihza@gmail.com>
2024-06-25 23:30:59 +0700

    Variable Environment API Token

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,20 +1,42 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React from 'react'
+import { View, Text, Button } from 'react-native'
+import { API_URL, API_ACCESS_TOKEN } from '@env'
 
-export default function MovieDetail({ navigation }: any): JSX.Element {
-  
+const MovieDetail = ({ navigation }: any): any => {
+  const fetchData = (): void => {
+    if (API_URL == null || API_ACCESS_TOKEN.length == null) {
+      throw new Error('ENV not found')
+    }
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+      },
+    }
+
+    fetch(API_URL, options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
   return (
-    <View style={styles.movieDetailContainer}>
-      <Text>Movie Detail</Text>
-      <Button title="Kembali" onPress={() => navigation.pop()} />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Movie Detail Page</Text>
+      <Button
+        title="Fetch Data"
+        onPress={() => {
+          fetchData()
+        }}
+      />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  movieDetailContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default MovieDetail
